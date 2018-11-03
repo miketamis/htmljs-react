061528f76626dbbd8dac9d0f604d5b14ac362db7
Mike Tamis <tamis.mike@gmail.com>
2018-11-04 12:06:48 +1300

    Added react build

---
+++ precache-manifest.1d535ddfaec82800e046c062fb50d67a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "d8a71c2aa16a819b1000",
+    "url": "/static/js/main.d8a71c2a.chunk.js"
+  },
+  {
+    "revision": "4fc0955f76e3c3f23e7f",
+    "url": "/static/js/1.4fc0955f.chunk.js"
+  },
+  {
+    "revision": "d8a71c2aa16a819b1000",
+    "url": "/static/css/main.68f06523.chunk.css"
+  },
+  {
+    "revision": "4fc0955f76e3c3f23e7f",
+    "url": "/static/css/1.241a5b25.chunk.css"
+  },
+  {
+    "revision": "1ce98270909b2f20364661e8ffcc7a8c",
+    "url": "/index.html"
+  }
+];
