62174124d8fbb0b5d48e46369b637960bf6ecc6d
Mike Tamis <tamis.mike@gmail.com>
2018-11-04 18:23:45 +1300

    New Build

---
+++ precache-manifest.84450c73ec50ea5717a01429a27affa3.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "497139a8f0c08147c687",
+    "url": "https://miketamis.github.io/htmljs-react/static/js/runtime~main.497139a8.js"
+  },
+  {
+    "revision": "573f62fb020d61ad81dc",
+    "url": "https://miketamis.github.io/htmljs-react/static/js/main.573f62fb.chunk.js"
+  },
+  {
+    "revision": "54ba706e005ca31a4f99",
+    "url": "https://miketamis.github.io/htmljs-react/static/js/1.54ba706e.chunk.js"
+  },
+  {
+    "revision": "573f62fb020d61ad81dc",
+    "url": "https://miketamis.github.io/htmljs-react/static/css/main.290e12b5.chunk.css"
+  },
+  {
+    "revision": "54ba706e005ca31a4f99",
+    "url": "https://miketamis.github.io/htmljs-react/static/css/1.540bfbd5.chunk.css"
+  },
+  {
+    "revision": "0bd309315532e8fa186fbc7cf6ffb85a",
+    "url": "https://miketamis.github.io/htmljs-react/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.7f6906aa42218e88b732538698b66992.js"
+  "https://miketamis.github.io/htmljs-react/precache-manifest.84450c73ec50ea5717a01429a27affa3.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/index.html", {
+workbox.routing.registerNavigationRoute("https://miketamis.github.io/htmljs-react/index.html", {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
