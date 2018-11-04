5238cc5bcbee3c45ca03195af52548e212248d25
Mike Tamis <tamis.mike@gmail.com>
2018-11-04 18:18:25 +1300

    New build

---
+++ precache-manifest.7f6906aa42218e88b732538698b66992.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "f217d2a26a39f112be14",
+    "url": "/static/js/main.f217d2a2.chunk.js"
+  },
+  {
+    "revision": "74abc30bdf8763d85094",
+    "url": "/static/js/1.74abc30b.chunk.js"
+  },
+  {
+    "revision": "f217d2a26a39f112be14",
+    "url": "/static/css/main.290e12b5.chunk.css"
+  },
+  {
+    "revision": "74abc30bdf8763d85094",
+    "url": "/static/css/1.540bfbd5.chunk.css"
+  },
+  {
+    "revision": "3718378932c5e15efbf0b7adf5d7ec41",
+    "url": "/index.html"
+  }
+];
